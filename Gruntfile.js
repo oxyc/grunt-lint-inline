f2a28da59a03e58c744ffd47475292b5f2bc6671
Tobias Lundin <tobias.lundin@gmail.com>
2013-10-01 12:59:59 +0200

    adding watch plugin for automated test runs

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,12 +9,19 @@ module.exports = function (grunt) {
     },
     nodeunit: {
       test: ['test/test.js']
+    },
+    watch: {
+      scripts: {
+        files: ['<%= jshint.src %>'],
+        tasks: ['jshint', 'test']
+      }
     }
   });
 
   grunt.loadTasks('tasks');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('test', ['nodeunit']);
   grunt.registerTask('default', ['jshint', 'test']);
 };
